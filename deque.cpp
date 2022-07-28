4385bdb006c152c4656966bb1887ab33a3fe8792
Ayush Agarwal <95439195+ayush-agarwal-190@users.noreply.github.com>
2022-07-28 22:13:08 +0530

    Add files via upload

---
+++ deque.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+deque<int> dq;
+dq.push_back(1);
+dq.push_back(2);
+dq.push_back(3);
+dq.push_back(4);
+int i;
+for(auto i: dq)
+ cout<<i<<" ";
+ cout<<"\n";
+
+ dq.pop_back();
+for(auto i: dq)
+ cout<<i<<" ";
+ cout<<"\n";
+
+ dq.pop_front();
+for(auto i: dq)
+ cout<<i<<" ";
+ cout<<"\n";
+
+return 0;
+
+}
